6cf5997d180ef5ce4a438bd785ff12b1e468a52b
tushardate <tushardate@gmail.com>
2017-03-23 08:29:38 -0700

    Added new design and color scheme to the app

--- GolfPractice/ViewController.swift
+++ GolfPractice/ViewController.swift
@@ -17,14 +17,6 @@ extension Array {
 }
 
 class ViewController: UIViewController {
-
-    let clubs = ["Driver", "3 wood", "5 wood", "4 hybrid", "4 iron", "5 iron", "6 iron", "7 iron", "8 iron", "9 iron", "PW", "SW", "LW"]
-    let shotShape = ["Baby Fade", "Fade", "Baby Draw", "Draw", "Slice", "Hook", "Straight"]
-    let shotTrajectory = ["High", "Low", "Medium"]
-    
-    var time = 10;
-    var timer = Timer()
-    
     @IBOutlet weak var clubsDisplay: UILabel!
     @IBOutlet weak var shotShapeDisplay: UILabel!
     @IBOutlet weak var shotTrajectoryDisplay: UILabel!
@@ -32,6 +24,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var shotTrajectoryImage: UIImageView!
     @IBOutlet weak var shotShapeImage: UIImageView!
     
+    let clubs = ["Driver", "3 wood", "5 wood", "4 hybrid", "4 iron", "5 iron", "6 iron", "7 iron", "8 iron", "9 iron", "PW", "SW", "LW"]
+    let shotShape = ["Baby Fade", "Fade", "Baby Draw", "Draw", "Slice", "Hook", "Straight"]
+    let shotTrajectory = ["High", "Low", "Medium"]
+    
+    var time = 2;
+    var timer = Timer()
+    
+    
+    let onColor  = UIColor(red:1.00, green:0.34, blue:0.42, alpha:1.0)
+    let offColor = UIColor(red:0.31, green:0.87, blue:0.76, alpha:1.0)
+    
+
     @IBAction func generateButton(_ sender: UIButton) {
         resetTimer()
         generateShot()
@@ -70,16 +74,19 @@ class ViewController: UIViewController {
     }
     
     override func viewDidLoad() {
+        clubsLockSwitch.onTintColor = onColor
+        clubsLockSwitch.tintColor = offColor
+        clubsLockSwitch.layer.cornerRadius = 16
+        clubsLockSwitch.backgroundColor = offColor
         clubsLockSwitch.transform = CGAffineTransform(scaleX: 0.70, y: 0.70)
+        
+        
         super.viewDidLoad()
         generateShot()
         runTimer()
     }
-//
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//        // Dispose of any resources that can be recreated.
-//    }
+
+
 
 }
 
